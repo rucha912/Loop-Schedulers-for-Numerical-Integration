d9fdcbf34c6dd0ab7248892632e85e4a9627284e
rkadam <rkadam@uncc.edu>
2017-10-04 12:28:27 -0400

    deleted endl from stdout

--- dynamic_sched.cpp
+++ dynamic_sched.cpp
@@ -1,4 +1,10 @@
 #include <iostream>
+#include<string.h>
+#include<pthread.h>
+#include<stdlib.h>
+#include<math.h>
+#include<chrono>
+
 
 #ifdef __cplusplus
 extern "C" {
@@ -13,13 +19,217 @@ float f4(float x, int intensity);
 }
 #endif
 
+
+float global_result = 0, x_int;
+float *thread_ans;
+clock_t start, end;
+float a, b;
+unsigned long n;
+double cpu_time;
+int func, intensity, granularity, work_done = 0, nbthreads;
+char* sync;
+int startloop=0, endloop;
+float result = 0, x_val = 0;
+
+pthread_mutex_t loop_locks, global_result_lock, iteration_lock;
+
+int get_end(int start)
+{
+		if(start+granularity > n)
+			return n-1;
+		return (start+granularity)-1;
+}
+
+int get_start();
+{
+	return startloop;
+}
+    
+void* integrate_iteration_level(void *unused)
+{
+	float loop_result;
+	int loop_end, loop_start;
+	pthread_t thread_id = pthread_self();
+	while(work_done != 1)
+	{
+		pthread_mutex_lock(&loop_locks);
+		loop_end = get_end(startloop);
+		pthread_mutex_unlock(&loop_locks);
+		std::cout<<"Thread ID:"<<thread_id<<"Start:"<<startloop<<"End:"<<endloop<<std::endl;
+		for(int i = startloop; i < loop_end; i++)
+    	{	
+    		pthread_mutex_lock(&iteration_lock);
+			x_int = (a + (i + 0.5) * ((b - a) / (float)n));
+			x_val = x_val + x_int;
+			switch(func)
+        	{
+      			case 1: global_result = f1(x_val, intensity) * ((b - a)/n);
+						break;
+        		case 2: global_result = f2(x_val, intensity) * ((b - a)/n);
+						break;
+        	  	case 3: global_result = f3(x_val, intensity) * ((b - a)/n);
+						break;
+      		  	case 4: global_result = f4(x_val, intensity) * ((b - a)/n);
+						break;
+        	  	default: std::cout<<"\nWrong function id"<<std::endl;
+      		}
+      		pthread_mutex_unlock(&iteration_lock);	
+		}
+		if(endloop >= n)
+		{
+			work_done = 1;
+			pthread_exit(NULL);
+		}
+		else
+		{
+			pthread_mutex_lock(&loop_locks);
+			startloop = endloop + 1;
+			pthread_mutex_unlock(&loop_locks);
+		}
+	}
+}
+
+void* integrate_chunk_level(void *unused)
+{
+	float chunk_result = 0;
+	while(work_done != 1)
+	{
+		pthread_mutex_lock(&loop_locks);
+		endloop = get_end(startloop);
+		pthread_mutex_unlock(&loop_locks);
+		
+		std::cout<<"Start:"<<startloop<<"End:"<<endloop<<std::endl;
+		for(int i = startloop; i < endloop; i++)
+    	{	
+			x_int = (a + (i + 0.5) * ((b - a) / (float)n));
+			x_val = x_val + x_int;
+			switch(func)
+        	{
+      			case 1: chunk_result = f1(x_val, intensity) * ((b - a)/n);
+						break;
+        		case 2: chunk_result = f2(x_val, intensity) * ((b - a)/n);
+						break;
+        	  	case 3: chunk_result = f3(x_val, intensity) * ((b - a)/n);
+						break;
+      		  	case 4: chunk_result = f4(x_val, intensity) * ((b - a)/n);
+						break;
+        	  	default: std::cout<<"\nWrong function id"<<std::endl;
+      		}
+      		pthread_mutex_lock(&global_result_lock);
+      		global_result = chunk_result;
+      		pthread_mutex_unlock(&global_result_lock);	
+		}
+		
+		if(endloop == n-1)
+		{
+			work_done = 1;
+			pthread_exit(NULL);
+		}
+		else
+		{
+			pthread_mutex_lock(&loop_locks);
+			startloop = endloop;
+			pthread_mutex_unlock(&loop_locks);
+		}
+	}
+}
+
+void* integrate_thread_level(void* unused)
+{
+	uintptr_t id = (uintptr_t)unused;
+	while(work_done != 1)
+	{
+		pthread_mutex_lock(&loop_locks);
+		endloop = get_end(startloop);
+		pthread_mutex_unlock(&loop_locks);
+	
+		for(int i = startloop; i < endloop; i++)
+    	{	
+			x_int = (a + (i + 0.5) * ((b - a) / (float)n));
+			x_val = x_val + x_int;
+			switch(func)
+        	{
+        		std::cout<<"Index:"<<id<<std::endl;
+      			case 1: thread_ans[id] += f1(x_val, intensity) * ((b - a)/n);
+						break;
+        		case 2: thread_ans[id] += f2(x_val, intensity) * ((b - a)/n);
+						break;
+        	  	case 3: thread_ans[id] = f3(x_val, intensity) * ((b - a)/n);
+						break;
+      		  	case 4: thread_ans[id] = f4(x_val, intensity) * ((b - a)/n);
+						break;
+        	  	default: std::cout<<"\nWrong function id"<<std::endl;
+      		}
+		}
+		if(endloop == n)
+		{
+			work_done = 1;
+			pthread_exit(NULL);
+		}
+		else
+		{
+			pthread_mutex_lock(&loop_locks);
+			startloop = endloop;
+			pthread_mutex_unlock(&loop_locks);
+		}
+	}
+}
+
   
 int main (int argc, char* argv[]) {
 
-  if (argc < 9) {
-    std::cerr<<"usage: "<<argv[0]<<" <functionid> <a> <b> <n> <intensity> <nbthreads> <sync> <granularity>"<<std::endl;
-    return -1;
-  }
+	func = atoi(argv[1]);
+    a = atof(argv[2]);
+    b = atof(argv[3]);
+    n = atof(argv[4]);
+    intensity = atoi(argv[5]);
+    nbthreads = atoi(argv[6]);
+    sync = argv[7];
+	granularity = atoi(argv[8]);
+    pthread_t threads[nbthreads];
+    
+    auto clock_start = std::chrono::system_clock::now();
+    
+    if( strcmp(sync, "iteration") == 0)
+    {
+ 		for(int i = 0; i < nbthreads; i++)
+ 		{
+ 			pthread_create(&threads[i], NULL, integrate_iteration_level, NULL);
+ 		}
+ 	}
+ 	
+    else if( strcmp(sync, "thread") == 0)
+    {
+ 		for(int i = 0; i < nbthreads; i++)
+ 		{
+ 			pthread_create(&threads[i], NULL, integrate_thread_level, NULL);
+ 		}
+ 	}
+ 	else if( strcmp(sync, "chunk") == 0)
+ 	{
+ 		for(int i = 0; i < nbthreads; i++)
+ 		{
+ 			pthread_create(&threads[i], NULL, integrate_chunk_level, NULL);
+ 		}
+ 	}
+	for ( int i = 0; i < nbthreads; i++)
+    {
+        pthread_join(threads[i], NULL);
+    }
+    
+    if( strcmp(sync, "thread") == 0)
+    {
+ 		for(int i = 0; i < nbthreads; i++)
+ 		{
+ 			global_result += thread_ans[i];
+ 		}
+ 	}
+ 	
+ 	auto clock_end = std::chrono::system_clock::now();
+    std::chrono::duration<double>diff = clock_end - clock_start;
+    std::cout<<global_result<<std::endl;
+    std::cerr<<diff.count()<<std::endl;
+    
 
   return 0;
 }
--- static_sched.cpp
+++ static_sched.cpp
@@ -37,19 +37,19 @@ void* integrate_thread_level(void * argument)
     for(int i = arg->start; i < arg->end; i++)
     {
     	  
-          arg->x_int = (arg->a + (i + 0.5) * ((arg->b - arg->a) / (float)arg->n));
-	  arg->x_val = arg->x_val + arg->x_int;
-	  switch(arg->func)
-          {
-      	      case 1: arg->result = f1(arg->x_val, arg->intensity) * ((arg->b - arg->a)/arg->n);
-	      break;
-              case 2: arg->result = f2(arg->x_val, arg->intensity) * ((arg->b - arg->a)/arg->n);
-	      break;
-              case 3: arg->result = f3(arg->x_val, arg->intensity) * ((arg->b - arg->a)/arg->n);
-	      break;
-      	      case 4: arg->result = f4(arg->x_val, arg->intensity) * ((arg->b - arg->a)/arg->n);
-	      break;
-              default: std::cout<<"\nWrong function id"<<std::endl;
+    	arg->x_int = (arg->a + (i + 0.5) * ((arg->b - arg->a) / (float)arg->n));
+		arg->x_val = arg->x_val + arg->x_int;
+		switch(arg->func)
+    	{
+      		case 1: arg->result = f1(arg->x_val, arg->intensity) * ((arg->b - arg->a)/arg->n);
+	      	break;
+          	case 2: arg->result = f2(arg->x_val, arg->intensity) * ((arg->b - arg->a)/arg->n);
+	      	break;
+          	case 3: arg->result = f3(arg->x_val, arg->intensity) * ((arg->b - arg->a)/arg->n);
+	      	break;
+      	  	case 4: arg->result = f4(arg->x_val, arg->intensity) * ((arg->b - arg->a)/arg->n);
+	      	break;
+            default: std::cout<<"\nWrong function id"<<std::endl;
       	 }
     }
     pthread_exit(NULL);
@@ -61,28 +61,28 @@ void* integrate_iteration_level(void * argument)
     struct arguments* arg = (struct arguments* )argument;
   
     for(int i = arg->start; i < arg->end; i++)
-      {
-	arg->x_int = (arg->a + (i + 0.5) * ((arg->b - arg->a) / (float)arg->n));
-	arg->x_val = arg->x_val + arg->x_int;
-	switch(arg->func)
-        {
+    {
+		arg->x_int = (arg->a + (i + 0.5) * ((arg->b - arg->a) / (float)arg->n));
+		arg->x_val = arg->x_val + arg->x_int;
+		switch(arg->func)
+    	{
 	 
-      	  case 1: arg->result =  f1(arg->x_val, arg->intensity) * ((arg->b - arg->a)/arg->n);
+    		case 1: arg->result =  f1(arg->x_val, arg->intensity) * ((arg->b - arg->a)/arg->n);
 	
-		break;
-          case 2: arg->result = f2(arg->x_val, arg->intensity) * ((arg->b - arg->a)/arg->n);
-		break;
-          case 3: arg->result = f3(arg->x_val, arg->intensity) * ((arg->b - arg->a)/arg->n);
-		break;
-      	  case 4: arg->result = f4(arg->x_val, arg->intensity) * ((arg->b - arg->a)/arg->n);
-		break;
-          default: std::cout<<"\nWrong function id"<<std::endl;
+			break;
+          	case 2: arg->result = f2(arg->x_val, arg->intensity) * ((arg->b - arg->a)/arg->n);
+			break;
+          	case 3: arg->result = f3(arg->x_val, arg->intensity) * ((arg->b - arg->a)/arg->n);
+			break;
+      	  	case 4: arg->result = f4(arg->x_val, arg->intensity) * ((arg->b - arg->a)/arg->n);
+			break;
+        	default: std::cout<<"\nWrong function id"<<std::endl;
       	}
       }
       pthread_mutex_lock(&global_result_lock);
       global_result = global_result + arg->result;
       pthread_mutex_unlock(&global_result_lock);
-     pthread_exit(NULL);
+      pthread_exit(NULL);
 }
   
 int main (int argc, char* argv[]) {
@@ -106,6 +106,7 @@ int main (int argc, char* argv[]) {
     struct arguments arg[nbthreads];
 
     float results[nbthreads] = {0};
+    pthread_mutex_init(&global_result_lock, NULL);
    
     auto clock_start = std::chrono::system_clock::now();
 
@@ -154,8 +155,8 @@ int main (int argc, char* argv[]) {
    
     auto clock_end = std::chrono::system_clock::now();
     std::chrono::duration<double>diff = clock_end - clock_start;
-    std::cout<<global_result<<std::endl;
-    std::cerr<<diff.count()<<std::endl;
+    std::cout<<global_result;
+    std::cerr<<diff.count();
   
   return 0;
   }
