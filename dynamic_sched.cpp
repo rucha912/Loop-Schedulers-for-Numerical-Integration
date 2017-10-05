af102d5bf934fcc7c653e6faa8d988e9487d1612
rkadam <rkadam@uncc.edu>
2017-10-05 12:08:17 -0400

    completed dynamic scheduling

--- dynamic_sched.cpp
+++ dynamic_sched.cpp
@@ -28,10 +28,16 @@ unsigned long n;
 double cpu_time;
 int func, intensity, granularity, work_done = 0, nbthreads;
 char* sync;
-int startloop=0, endloop;
+unsigned long startloop=0, endloop;
 float result = 0, x_val = 0;
 
 pthread_mutex_t loop_locks, global_result_lock, iteration_lock;
+
+struct thread_arg
+{
+	float thread_result, x_val=0, x_int;
+	unsigned long start, end;
+};
 
 int get_end(int start)
 {	
@@ -52,17 +58,18 @@ int get_start()
 	pthread_mutex_unlock(&loop_locks);
 	return temp;
 }
+
+//This function does the integration using iteration level mutual exclusion. The critical section is in the innermost loop.
     
 void* integrate_iteration_level(void *unused)
 {
 	float loop_result;
-	int loop_end, loop_start;
+	unsigned long loop_end, loop_start;
 	pthread_t thread_id = pthread_self();
 	while(work_done != 1)
 	{
 		loop_start = get_start();
 		loop_end = get_end(loop_start);
-		std::cout<<"Start:"<<loop_start<<"End:"<<loop_end<<std::endl;
 		for(int i = loop_start; i <= loop_end; i++)
     	{	
     		pthread_mutex_lock(&iteration_lock);
@@ -85,16 +92,17 @@ void* integrate_iteration_level(void *unused)
 	}
 }
 
+//This function does integration using chunk level mutual exclusion. The critical section is in the while loop for every computing thread.
+
 void* integrate_chunk_level(void *unused)
 {
 	float chunk_result = 0;
-	int loop_end, loop_start;
+	unsigned long loop_end, loop_start;
 	while(work_done != 1)
 	{
 		loop_start = get_start();
 		loop_end = get_end(loop_start);
-		std::cout<<"Start:"<<loop_start<<"End:"<<loop_end<<std::endl;
-		for(int i = loop_start; i < loop_end; i++)
+		for(int i = loop_start; i <= loop_end; i++)
     	{	
 			x_int = (a + (i + 0.5) * ((b - a) / (float)n));
 			x_val = x_val + x_int;
@@ -118,35 +126,36 @@ void* integrate_chunk_level(void *unused)
 	}
 }
 
-void* integrate_thread_level(void* unused)
+//This function does integration using thread level mutual exclusion where the every thread computes the result in the local variable and aggregates the result in the end.
+
+void* integrate_thread_level(void* thread_args)
 {
 	float thread;
-	int loop_end, loop_start;
+	struct thread_arg* arg = (struct thread_arg *)thread_args;
 	while(work_done != 1)
 	{
-		loop_start = get_start();
-		loop_end = get_end(loop_start);
-		std::cout<<"Start:"<<loop_start<<"End:"<<loop_end<<std::endl;
-	
-		for(int i = loop_start; i < loop_end; i++)
+		arg->start = get_start();
+		arg->end = get_end(arg->start);
+
+		for(int i = arg->start; i <= arg->end; i++)
     	{	
-			x_int = (a + (i + 0.5) * ((b - a) / (float)n));
-			x_val = x_val + x_int;
+			arg->x_int = (a + (i + 0.5) * ((b - a) / (float)n));
+			arg->x_val = arg->x_val + arg->x_int;
 			switch(func)
         	{
-      			case 1: thread += f1(x_val, intensity) * ((b - a)/n);
+      			case 1: arg->thread_result = f1(arg->x_val, intensity) * ((b - a)/n);
 						break;
-        		case 2: thread += f2(x_val, intensity) * ((b - a)/n);
+        		case 2: arg->thread_result = f2(arg->x_val, intensity) * ((b - a)/n);
 						break;
-        	  	case 3: thread += f3(x_val, intensity) * ((b - a)/n);
+        	  	case 3: arg->thread_result = f3(arg->x_val, intensity) * ((b - a)/n);
 						break;
-      		  	case 4: thread += f4(x_val, intensity) * ((b - a)/n);
+      		  	case 4: arg->thread_result = f4(arg->x_val, intensity) * ((b - a)/n);
 						break;
         	  	default: std::cout<<"\nWrong function id"<<std::endl;
       		}
 		}
 	}
-	pthread_exit((void *)&thread);
+	pthread_exit(NULL);
 }
 
   
@@ -162,10 +171,10 @@ int main (int argc, char* argv[])
     sync = argv[7];
 	granularity = atoi(argv[8]);
     pthread_t *threads;
-	float *thread_ans;
+	struct thread_arg* arg;
 	
     threads = (pthread_t *)malloc(nbthreads * sizeof(pthread_t));
-    thread_ans = (float *)malloc(nbthreads * sizeof(float));
+    arg = (struct thread_arg*)malloc(nbthreads * sizeof(thread_arg));
     
     auto clock_start = std::chrono::system_clock::now();
     
@@ -181,7 +190,7 @@ int main (int argc, char* argv[])
     {
  		for(int i = 0; i < nbthreads; i++)
  		{
- 			pthread_create(&threads[i], NULL, integrate_thread_level, NULL);
+ 			pthread_create(&threads[i], NULL, integrate_thread_level, (void *)&(arg[i]));
  		}
  	}
  	else if( strcmp(sync, "chunk") == 0)
@@ -195,12 +204,12 @@ int main (int argc, char* argv[])
  	{
 		for ( int i = 0; i < nbthreads; i++)
     	{
-        	pthread_join(threads[i], (void **)&thread_ans[i]);
+        	pthread_join(threads[i], NULL);
     	}
-    	for(int i = 0; i < nbthreads; i++)
- 		{
- 			global_result += thread_ans[i];
- 		}
+    	for( int k = 0; k < nbthreads; k++)
+    	{
+    	    global_result += arg[k].thread_result;
+    	}
     }
     else
     {
@@ -212,8 +221,8 @@ int main (int argc, char* argv[])
  	
  	auto clock_end = std::chrono::system_clock::now();
     std::chrono::duration<double>diff = clock_end - clock_start;
-    std::cout<<global_result<<std::endl;
-    std::cerr<<diff.count()<<std::endl;
+    std::cout<<global_result;
+    std::cerr<<diff.count();
     
 
   return 0;
